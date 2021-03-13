b9f61fe77231813df0b11e134b5f52e2f8593c4c
Jorge Luis Garnica Blanco <jorgegarba@gmail.com>
2021-03-13 11:28:48 -0500

    ejercicios con cambio de temas en javascript

--- semana4/dia6/01-element3.js
+++ semana4/dia6/01-element3.js
@@ -2,8 +2,22 @@ function qs(selector) {
   return document.querySelector(selector);
 }
 
+const qsall = _ => document.querySelectorAll(_);
+
 
 //modificando los estilos de un elemento
 // let body = qs("body");
 // body.style.backgroundColor = "#222";
 // body.style.color = "#eee";
+
+let lis = qsall(".header__item");
+lis = Array.from(lis);
+
+lis.forEach((headerItem) => {
+  // configurando el click de cada <li>
+  headerItem.onclick = function () {
+    const itemActive = qs(".header__item.active");
+    itemActive.classList.remove("active");
+    headerItem.classList.add("active");
+  }
+});
