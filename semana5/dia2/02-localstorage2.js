923665b86a76649c32e4ac993636b191e92cbb48
Jorge Luis Garnica Blanco <jorgegarba@gmail.com>
2021-03-16 22:06:49 -0500

    Ejercicios con localstorage

--- semana5/dia2/02-localstorage2.js
+++ semana5/dia2/02-localstorage2.js
@@ -1,6 +1,7 @@
 const formulario = document.getElementById("formulario");
 const inputTitulo = document.getElementById("inputTitulo");
 const inputDescripcion = document.getElementById("inputDescripcion");
+const tbody = document.getElementById("tbody");
 
 let arrayTODOS = [];
 
@@ -23,5 +24,44 @@ formulario.onsubmit = function (e) {
  * Función que tomar el arrayTODOS y lo dibuja en el <tbody>
  */
 const dibujarTODOS = () => {
+  // limpiando el tbody antes de hacer nada
+  tbody.innerHTML = "";
 
+  arrayTODOS.forEach((objTODO, i) => {
+    const tr = document.createElement("tr");
+    const tdNro = document.createElement("td");
+    const tdTarea = document.createElement("td");
+    const tdAcciones = document.createElement("td");
+    const btnEliminar = document.createElement("button");
+    btnEliminar.innerText = "Eliminar"
+    // condigurando el evento click para cada botón eliminar
+    btnEliminar.onclick = () => {
+      eliminarTODO(i);
+    }
+
+    tdNro.innerText = i + 1;
+    tdTarea.innerHTML = /*html */`
+      <h3>${objTODO.titulo}</h3>
+      <p>${objTODO.descripcion}</p>`;
+    tdAcciones.appendChild(btnEliminar);
+
+    tr.appendChild(tdNro);
+    tr.appendChild(tdTarea);
+    tr.appendChild(tdAcciones);
+
+    tbody.appendChild(tr);
+  });
+}
+
+
+const eliminarTODO = posicion => {
+
+  /**
+   * array.splice(posicion,nro_elementos)
+   * Elimina "nro_elementos " de un arreglo a partir de la posición "posicion"
+   * 
+   * OPCIONALMENTE RETORNA EL ELEMENTO ELIMINADO
+   */
+  arrayTODOS.splice(posicion, 1);
+  dibujarTODOS();
 }
