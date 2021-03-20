e2c1042a5f43801655565db41e83ca6d41bd8bb4
Jorge Luis Garnica Blanco <jorgegarba@gmail.com>
2021-03-19 20:14:01 -0500

    pre-finalizacion ejericcio de peliculas

--- semana5/dia5/01-peliculas-app.js
+++ semana5/dia5/01-peliculas-app.js
@@ -1,3 +1,74 @@
 import { peliculas, titulo } from "./data-peliculas.js"
 
 
+const contenedor = document.querySelector("#contenedor");
+
+const fillMovies = () => {
+
+
+  peliculas.forEach((objPelicula) => {
+    // creando la columna
+    const col = document.createElement("div");
+    col.classList.add("col-md-3");
+    // creando el card
+    const card = document.createElement("div")
+    card.classList.add("card");
+    card.innerHTML = `<img src="https://image.tmdb.org/t/p/w500/${objPelicula.poster_path}" alt="" class="card-img-top">`;
+
+    // creando el cardbody
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+    cardBody.innerHTML = `<h4 class="card-title">${objPelicula.original_title}</h4>`;
+
+    // creando el párrafo de la sinopsis
+    const cardText = document.createElement("p");
+    cardText.classList.add("card-text");
+    cardText.innerText = `${objPelicula.overview}`;
+
+    //creando el link de leer mas!
+    const cardLink = document.createElement("a");
+    cardLink.innerText = "leer mas...";
+    cardLink.href = "#";
+
+    // creando el ul de las características
+    const cardFeatures = document.createElement("ul");
+    cardFeatures.classList.add("movie__features");
+
+    const iconoAdulto = objPelicula.adult ? `<i class="fas fa-ban text-danger"></i>` : ` <i class="fas fa-check text-success"></i>`;
+    const listaString = `
+    <li>
+      <small class="text-muted">
+        <i class="fas fa-calendar"></i> ${objPelicula.release_date}
+      </small>
+    </li>
+    <li>
+      <small class="text-muted">
+        <i class="fas fa-star text-warning"></i> ${objPelicula.vote_average}
+      </small>
+    </li>
+    <li>
+      <small class="text-muted">
+        <i class="fas fa-heart text-danger"></i> ${objPelicula.vote_count}
+      </small>
+    </li>
+    <li>
+      <small class="text-muted">
+        +18
+        ${iconoAdulto}
+      </small>
+    </li>`;
+    cardFeatures.innerHTML = listaString;
+
+
+    col.appendChild(card);
+    card.appendChild(cardBody);
+    cardText.appendChild(cardLink);
+    cardBody.appendChild(cardText);
+    cardBody.appendChild(cardFeatures);
+    contenedor.appendChild(col);
+
+  });
+
+
+}
+fillMovies();
